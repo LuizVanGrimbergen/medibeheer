e2af86622e959414b1814959eab6af60702bfa2f
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-05-01 11:20:11 +0200

    Align dashboard with updated layout

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,30 +1,15 @@
 <script setup lang="ts">
 import { Head } from '@inertiajs/vue3';
+import { useI18n } from 'vue-i18n';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+
+const { t } = useI18n();
 </script>
 
 <template>
-    <Head title="Dashboard" />
+    <Head>
+        <title>{{ t('app.dashboard.title') }}</title>
+    </Head>
 
-    <AuthenticatedLayout>
-        <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
-            >
-                Dashboard
-            </h2>
-        </template>
-
-        <div class="py-12">
-            <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800"
-                >
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        You're logged in!
-                    </div>
-                </div>
-            </div>
-        </div>
-    </AuthenticatedLayout>
+    <AuthenticatedLayout />
 </template>
