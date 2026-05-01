907f09034892a546b50d80b3c717e574cc007332
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-05-01 10:19:42 +0200

    Add mail and auth translations (en/nl)

---
+++ lang/nl/mail.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'verify_email' => [
+        'subject' => 'Bevestig je e-mailadres',
+        'greeting' => 'Welkom bij Medibeheer!',
+        'line' => 'Bevestig je e-mailadres om je account te activeren.',
+        'action' => 'E-mailadres bevestigen',
+        'footer' => 'Als je dit account niet hebt aangemaakt, moet je dit negeren.',
+    ],
+    'reset_password' => [
+        'subject' => 'Reset je wachtwoord',
+        'greeting' => 'Hallo!',
+        'line' => 'Je ontvangt deze e-mail omdat we een wachtwoord reset hebben ontvangen voor jouw account.',
+        'action' => 'Wachtwoord resetten',
+        'footer' => 'Als je geen reset hebt aangevraagd, hoef je niets te doen.',
+    ],
+];
