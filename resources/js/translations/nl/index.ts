3d8e7423b8af6332c2b150df01c20f752b8f96c7
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-05-02 16:52:43 +0200

    add Dutch translations for family area

--- resources/js/translations/nl/index.ts
+++ resources/js/translations/nl/index.ts
@@ -1,11 +1,13 @@
 import app from './app';
 import auth from './auth';
+import family from './family';
 import patient from './patient';
 import profile from './profile';
 
 export default {
     app,
     auth,
+    family,
     patient,
     profile,
 };
