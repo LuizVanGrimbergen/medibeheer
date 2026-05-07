2f793c97459a41f297440e1ee43949956b3a8db1
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-05-07 15:21:01 +0200

    Family appointments screen service + resource

---
+++ app/Http/Resources/PatientAppointmentResource.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Appointment;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin Appointment */
+class PatientAppointmentResource extends JsonResource
+{
+    public static function collectForInertia(iterable $appointments): array
+    {
+        return static::collection($appointments)->resolve();
+    }
+
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'doctor_type' => $this->doctor_type->value,
+            'provider_name' => $this->provider_name,
+            'address' => $this->address,
+            'starts_at' => $this->starts_at->toIso8601String(),
+            'needs_transport' => (bool) $this->needs_transport,
+            'transport_status' => $this->resource->transportStatus(
+                (bool) ($this->has_pending_transport_invitation ?? false),
+            )?->value,
+            'transport_invited_family_ids' => $this->transportInvitations
+                ->pluck('family_id')
+                ->map(fn ($id) => (int) $id)
+                ->unique()
+                ->values()
+                ->all(),
+            'transport_family' => $this->resource->transportFamilyPayload(),
+            'notes' => $this->notes,
+            'doctor_visit_summary' => $this->doctor_visit_summary,
+            'cancellation_reason' => $this->cancellation_reason,
+            'status' => $this->status->value,
+        ];
+    }
+}
