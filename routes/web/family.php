95a5333e702a016f86d307b8d15bc1b70af48089
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-05-07 15:20:14 +0200

    Update family routes to new controllers

--- routes/web/family.php
+++ routes/web/family.php
@@ -1,9 +1,12 @@
 <?php
 
-use App\Http\Controllers\Family\AcceptFamilyInvitationController;
-use App\Http\Controllers\Family\FamilyOverviewController;
-use App\Http\Controllers\Family\FamilyUpdatesController;
-use App\Http\Controllers\Family\SwitchActivePatientController;
+use App\Http\Controllers\Family\Dashboard\FamilyAppointmentsController;
+use App\Http\Controllers\Family\Dashboard\FamilyOverviewController;
+use App\Http\Controllers\Family\Dashboard\FamilyUpdatesController;
+use App\Http\Controllers\Family\Invitations\AcceptAppointmentTransportInvitationController;
+use App\Http\Controllers\Family\Invitations\AcceptFamilyInvitationController;
+use App\Http\Controllers\Family\Invitations\DeclineAppointmentTransportInvitationController;
+use App\Http\Controllers\Family\Patients\SwitchActivePatientController;
 use App\Http\Middleware\EnsureFamilyMember;
 use App\Http\Middleware\RedirectIfEmailUnverified;
 use Illuminate\Auth\Middleware\Authenticate;
@@ -21,6 +24,7 @@ Route::middleware([
     ->name('family.')
     ->group(function (): void {
         Route::get('/', FamilyOverviewController::class)->name('overview');
+        Route::get('appointments', FamilyAppointmentsController::class)->name('appointments');
         Route::get('updates', FamilyUpdatesController::class)->name('updates');
 
         /* Family Invitations */
@@ -28,7 +32,17 @@ Route::middleware([
             ->middleware('throttle:family-invitation-accept')
             ->name('invitations.accept');
 
+        /* Appointment Transport Invitations */
+        Route::post('transport-invitations/{transportInvitation}/accept', AcceptAppointmentTransportInvitationController::class)
+            ->middleware('throttle:family-transport-invitation')
+            ->name('transport-invitations.accept');
+
+        Route::post('transport-invitations/{transportInvitation}/decline', DeclineAppointmentTransportInvitationController::class)
+            ->middleware('throttle:family-transport-invitation')
+            ->name('transport-invitations.decline');
+
         /* Active Patient */
         Route::post('patients/{patient}/switch', SwitchActivePatientController::class)
+            ->middleware('throttle:family-active-patient-switch')
             ->name('patients.switch');
     });
