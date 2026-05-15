27781c327b9245278281d33d9745d5f4a29c62a3
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-05-15 15:38:15 +0200

    Add optional strength to medication factory

--- database/factories/MedicationFactory.php
+++ database/factories/MedicationFactory.php
@@ -18,6 +18,11 @@ class MedicationFactory extends Factory
             'dose' => fake()->randomElement(['500 mg', '1 tablet', '10 ml', '2 druppels']),
             'dose_unit' => fake()->randomElement(MedicationDoseUnit::cases()),
             'type_medication' => fake()->randomElement(MedicationType::cases()),
+            'strength' => fake()->optional(0.35)->randomElement([
+                '500 mg per tablet',
+                '20 mg per ml',
+                '400 IE',
+            ]),
             'note' => fake()->optional(0.25)->realTextBetween(20, 120),
         ];
     }
