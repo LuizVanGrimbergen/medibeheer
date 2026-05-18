459fd331b8cd5560a270047c8cdc376b1c87f119
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-05-18 15:11:21 +0200

    Add family medications navigation and Dutch translations

---
+++ resources/js/translations/nl/family/medications.ts
@@ -0,0 +1,7 @@
+export default {
+    title: 'Medicatie',
+    heading: 'Medicatie',
+    notLinked:
+        'Je ziet hier het medicatieregister zodra je bent gekoppeld aan een patiënt. Gebruik je uitnodigingscode op het overzicht.',
+    empty: 'Er is nog geen medicatie geregistreerd voor de actieve patiënt.',
+};
