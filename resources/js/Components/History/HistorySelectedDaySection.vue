93b1d814c4450d7a2094ac98b8f512abebeb7424
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-05-18 15:10:24 +0200

    Add reusable HistoryMonthCalendar and HistorySelectedDaySection components

---
+++ resources/js/Components/History/HistorySelectedDaySection.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+    selectedDate: string | null;
+    heading: string;
+}>();
+</script>
+
+<template>
+    <section
+        v-if="selectedDate !== null"
+        class="scroll-mt-24 space-y-3"
+        tabindex="-1"
+    >
+        <h2 class="text-lg font-semibold text-text-heading">
+            {{ heading }}
+        </h2>
+        <slot />
+    </section>
+</template>
