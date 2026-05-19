188393a6b4301659077c19ba5f32abb90c412776
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-05-19 15:10:12 +0200

    Expose scrollIntoView on history selected day section

--- resources/js/Components/History/HistorySelectedDaySection.vue
+++ resources/js/Components/History/HistorySelectedDaySection.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
 defineProps<{
     selectedDate: string | null;
     heading: string;
 }>();
+
+const sectionRef = ref<HTMLElement | null>(null);
+
+function scrollIntoView(options?: ScrollIntoViewOptions): void {
+    sectionRef.value?.scrollIntoView(options);
+}
+
+defineExpose({
+    scrollIntoView,
+});
 </script>
 
 <template>
     <section
         v-if="selectedDate !== null"
+        ref="sectionRef"
         class="scroll-mt-24 space-y-3"
         tabindex="-1"
     >
