42f11d0a4f8514460c1a81a4d7f77893db307d08
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-05-21 14:26:49 +0200

    Add store for pending push medication marks on patient dashboard.

--- app/Http/Controllers/Patient/Dashboard/PatientDashboardController.php
+++ app/Http/Controllers/Patient/Dashboard/PatientDashboardController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\Patient\Concerns\AuthorizesPatientProfile;
 use App\Services\Medications\PatientScheduledIntakesQuery;
 use App\Support\Medications\MedicationIntakeClock;
+use App\Support\Medications\PatientRecentPushMedicationMarkStore;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -16,6 +17,7 @@ class PatientDashboardController extends Controller
 
     public function __construct(
         private readonly PatientScheduledIntakesQuery $scheduledIntakesQuery,
+        private readonly PatientRecentPushMedicationMarkStore $recentPushMarkStore,
     ) {}
 
     public function __invoke(Request $request): Response
@@ -34,6 +36,7 @@ class PatientDashboardController extends Controller
             'today_date' => $today->toDateString(),
             'today_checkin' => $todayCheckin?->toDashboardPayload(),
             'today_medication_intakes' => $this->scheduledIntakesQuery->forPatientOnDate($patient, $today),
+            'pending_push_medication_mark' => $this->recentPushMarkStore->peek($patient->id),
         ]);
     }
 }
