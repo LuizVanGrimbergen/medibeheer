d4c1e6061118542b757886227a9bc3d4ada55bee
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-05-22 15:25:51 +0200

    feat(family): add overview screen TypeScript types

---
+++ resources/js/lib/family/overview/familyPendingTransportAppointments.ts
@@ -0,0 +1,7 @@
+import type { FamilyAcceptedTransportAppointment } from '@/lib/family/overview/familyAcceptedTransportAppointments';
+
+export type FamilyPendingTransportAppointment = FamilyAcceptedTransportAppointment & {
+    invitation_id: number;
+    accept_url: string;
+    decline_url: string;
+};
