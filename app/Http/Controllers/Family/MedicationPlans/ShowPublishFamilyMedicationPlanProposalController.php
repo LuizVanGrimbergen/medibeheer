8cc6e4fc12e4539c2d30ea05ad3f1587be8ed33f
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-05-28 20:06:53 +0200

    Add family publish flow, link page and medication plan routes.

---
+++ app/Http/Controllers/Family/MedicationPlans/ShowPublishFamilyMedicationPlanProposalController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Family\MedicationPlans;
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Family\Concerns\AuthorizesFamilyProfile;
+use App\Models\MedicationPlanProposal;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class ShowPublishFamilyMedicationPlanProposalController extends Controller
+{
+    use AuthorizesFamilyProfile;
+
+    public function __invoke(
+        Request $request,
+        MedicationPlanProposal $medicationPlanProposal,
+    ): Response {
+        $family = $this->authorizeFamilyProfile($request);
+
+        $this->authorizeMedicationPlanProposalForFamily($family, $medicationPlanProposal);
+        $this->authorize('publish', $medicationPlanProposal);
+
+        $medicationPlanProposal->load(['items']);
+
+        $itemId = $request->integer('item');
+        $itemId = $itemId > 0
+            ? $itemId
+            : $medicationPlanProposal->items()->orderByDesc('sort_order')->value('id');
+
+        $firstItem = $medicationPlanProposal->items->sortByDesc('sort_order')->first();
+
+        return Inertia::render('Family/MedicationPlans/Publish', [
+            'proposal_id' => $medicationPlanProposal->id,
+            'medication_name' => $firstItem?->name,
+            'cancel_url' => route('family.medication-plans.edit', [
+                'medication_plan_proposal' => $medicationPlanProposal,
+                'item' => $itemId,
+                'summary' => 1,
+            ], absolute: false),
+        ]);
+    }
+}
