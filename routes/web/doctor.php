294514e167e26c0b223ac835c73129a14b02c31e
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-05-29 15:52:15 +0200

    Add doctor linked patient unlink destroy controller

--- routes/web/doctor.php
+++ routes/web/doctor.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Doctor\DoctorDashboardController;
 use App\Http\Controllers\Doctor\DoctorPatientsController;
+use App\Http\Controllers\Doctor\Invitations\AcceptIncomingDoctorInvitationController;
+use App\Http\Controllers\Doctor\Patients\DestroyDoctorLinkedPatientController;
 use App\Http\Middleware\EnsureDoctor;
 use App\Http\Middleware\RedirectIfEmailUnverified;
 use Illuminate\Auth\Middleware\Authenticate;
@@ -19,4 +21,12 @@ Route::middleware([
     ->group(function (): void {
         Route::get('/', DoctorDashboardController::class)->name('dashboard');
         Route::get('patients', DoctorPatientsController::class)->name('patients');
+
+        Route::delete('patients/links/{linkedPatient}', DestroyDoctorLinkedPatientController::class)
+            ->middleware('throttle:patient-care-team-unlink')
+            ->name('patients.links.destroy');
+
+        Route::post('invitations/{incomingDoctorInvitation}/accept-incoming', AcceptIncomingDoctorInvitationController::class)
+            ->middleware('throttle:doctor-invitation-accept')
+            ->name('invitations.incoming.accept');
     });
