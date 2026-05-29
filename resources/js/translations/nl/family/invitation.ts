0922cb132c17a177ec7f9a4f304fb96565aede43
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-05-29 15:52:12 +0200

    Remove legacy family invitation accept controller and request

--- resources/js/translations/nl/family/invitation.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    acceptHeading: 'Koppel met uitnodigingscode',
-    acceptToggle: 'Koppelen met uitnodigingscode tonen of verbergen',
-    acceptCollapsedSummary:
-        'Vul de code uit de uitnodigingsmail in.',
-    codeLabel: 'Code',
-    codePlaceholder: 'Plak uw 40-teken code',
-    submit: 'Koppelen',
-};
