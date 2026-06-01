f2e6cd3e4056818c85c72ec5d19fee81a68608f5
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-06-01 21:02:28 +0200

    Update strength-required copy for drops only

--- lang/en/medication.php
+++ lang/en/medication.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'strength_required' => 'Enter the strength for drops or injections.',
+    'strength_required' => 'Enter the strength for drops.',
     'schedule_course_max_days' => 'The course may not be longer than 366 days.',
     'schedule_intake_weekdays_required' => 'Choose at least one weekday.',
     'intake_slot_not_due_today' => 'This medication does not need to be taken today.',
