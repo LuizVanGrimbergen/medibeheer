5ea7ab523aef39d7df01139a353b6d763db62187
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-06-03 20:10:08 +0200

    Render a public guest home for unauthenticated visitors.
    Redirect signed-in users to their default home while serving an indexable marketing landing page for guests.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,16 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class HomeController extends Controller
 {
-    /**************************************/
-    /*              Actions */
-    /**************************************/
-
-    public function __invoke(): RedirectResponse
+    public function __invoke(Request $request): RedirectResponse|Response
     {
-        return redirect()->route('login');
+        $user = $request->user();
+
+        if ($user instanceof User) {
+            return redirect()->to($user->defaultAuthenticatedHomeUrl());
+        }
+
+        return Inertia::render('Guest/Home');
     }
 }
