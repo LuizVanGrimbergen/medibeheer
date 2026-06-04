814c3e1787c636c1368af9a82728d4636ebc7b60
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-06-04 15:17:08 +0200

    Adjust medication form wizard constants and deferred screen props typing.

--- resources/js/lib/patient/medications/screen/patientMedicationsScreenProps.ts
+++ resources/js/lib/patient/medications/screen/patientMedicationsScreenProps.ts
@@ -1,6 +1,6 @@
 import type { MedicationListItem, Paginated } from '@/lib/types';
 
 export type PatientMedicationsScreenProps = {
-    active_medications: Paginated<MedicationListItem>;
+    active_medications?: Paginated<MedicationListItem>;
     can_create_medication: boolean;
 };
