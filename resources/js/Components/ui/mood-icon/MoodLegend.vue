bfb7913965dff18a8789e6f62bd640bfb6938fce
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-06-05 13:59:58 +0200

    Add mood icon components and presentation.

---
+++ resources/js/Components/ui/mood-icon/MoodLegend.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n';
+import MoodIcon from '@/Components/ui/mood-icon/MoodIcon.vue';
+import { DAILY_MOOD_OPTIONS } from '@/lib/mood/dailyMoodPresentation';
+
+const { t } = useI18n();
+</script>
+
+<template>
+    <span
+        v-for="option in DAILY_MOOD_OPTIONS"
+        :key="option.mood"
+        class="text-text-heading inline-flex items-center gap-1.5 font-medium"
+    >
+        <MoodIcon :mood="option.mood" size="legend" />
+        {{ t(option.labelKey) }}
+    </span>
+</template>
