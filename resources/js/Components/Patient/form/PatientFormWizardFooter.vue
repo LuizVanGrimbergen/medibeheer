2ac03aa6ff6d37a41eb33df0742e3f661da873b9
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-06-05 13:59:51 +0200

    Add patient form wizard footer primitives.

---
+++ resources/js/Components/Patient/form/PatientFormWizardFooter.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { patientFormWizardFooterRowClass } from '@/lib/patient/patientShellDialogLayout';
+</script>
+
+<template>
+    <div :class="patientFormWizardFooterRowClass">
+        <slot />
+    </div>
+</template>
