192b5407912133e689ba8429aff98163f463e146
Luiz Van Grimbergen <luiz.vangrimbergen@gmail.com>
2026-06-05 14:00:07 +0200

    Move linked patients into FamilyPageShell.

--- resources/js/translations/nl/family/link.ts
+++ resources/js/translations/nl/family/link.ts
@@ -1,13 +1,11 @@
 export default {
     title: 'Koppel',
     heading: 'Koppel',
-    patientsHeading: 'Patiënten',
-    patientsToggle: 'Gekoppelde patiënten tonen of verbergen',
+    patientsHeading: 'Kies een patiënt',
+    patientsToggle: 'Kies een patiënt tonen of verbergen',
     patientsEmpty: 'Nog geen patiënten gekoppeld.',
     incomingInvitationsHeading: 'Verzoeken van patiënten',
     incomingInvitationsToggle: 'Koppelverzoeken tonen of verbergen',
-    incomingInvitationsIntro:
-        'Patiënten die u hebben uitgenodigd via uw e-mailadres. Accepteer een verzoek om gekoppeld te worden.',
     incomingInvitationsEmpty: 'Geen openstaande koppelverzoeken.',
     incomingInvitationsCollapsedOne: '1 open verzoek',
     incomingInvitationsCollapsedMany: '{count} open verzoeken',
